8989c56bdae4753e321ccf1cd99736fc2eb79319
Matt Daniel Brown <developer@mattdanielbrown.com>
2020-09-05 01:49:22 -0500

    Updated packages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,10 @@
-console.log('\'Allo \'Allo!');
+'use strict';
 
+function main() {
+  console.log('This is the main function');
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  main();
+});
